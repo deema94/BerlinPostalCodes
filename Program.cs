4b4015316e43b5193aa8bb4101701686082c9243
deema <damdom.94.8@gmail.com>
2022-05-06 14:21:12 +0200

    key "addr:postcode"

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace BerlinPostalCodes
@@ -11,43 +12,77 @@ namespace BerlinPostalCodes
     {
         static async Task Main(string[] args)
         {
-            // get the file
+            /// <summary>
+            /// get the file, download the file if it doesn't exist yet.
+            /// <summary>
+            await ToFile("http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf", "berlin-latest.osm.pbf");
             await using var fileStream = File.OpenRead("berlin-latest.osm.pbf");
 
-            // create source stream.
+            /// <summary>
+            /// create source stream.
+            /// <summary>
             var source = new PBFOsmStreamSource(fileStream);
 
-            // let's use linq to leave only objects with Type = Node and contains the key "postal_code"
+            /// <summary>
+            /// first tag "boundary=postal_code".
+            /// based on taginfo's statistics this tag is useful only in Germany because
+            /// the geographical distribution of this key here is very high.
+            /// "boundary=postal_code" will give us 192 postal codes in Berlin.
+            /// use linq to leave only objects with Type = Way and contains the tag "postal_code".
+            /// <summary>
             //var filtered = from osmGeo in source
-            //               where osmGeo.Tags.ContainsKey("postal_code") && osmGeo.Type == OsmSharp.OsmGeoType.Node 
+            //               where osmGeo.Tags.ContainsKey("postal_code") && osmGeo.Type == OsmSharp.OsmGeoType.Way
             //               select osmGeo;
 
-            // let's use linq to leave only objects with "boundary" = "postal_code"
+            /// <summary>
+            /// second key "addr:postcode"
+            /// in Karlsruhe Schema they use this key
+            /// "addr:postcode=*" will give us 193 postal codes in Berlin
+            /// use linq to leave only objects with Type = Node and contains the key "addr:postcode"
+            /// <summary>
             var filtered = from osmGeo in source
-                           where osmGeo.Tags.Contains("boundary", "postal_code") 
+                           where osmGeo.Tags.ContainsKey("addr:postcode") && osmGeo.Type == OsmSharp.OsmGeoType.Node
                            select osmGeo;
 
+            /// <summary>
+            /// add postal codes to a list then sort it and finally remove duplicates
+            /// <summary>
             List<string> postalCodes = new List<string>();
-
             foreach (var osmGeo in filtered)
             {
-                int code = 0;
-                if (int.TryParse(osmGeo.Tags.GetValue("postal_code"), out code))
-                {
-                    if (code >= 10115 && code <= 14199)
-                        postalCodes.Add(osmGeo.Tags.GetValue("postal_code"));
-                }
+                // show details on console
+                Console.WriteLine(osmGeo.Tags.GetValue("addr:street")
+                    + ", " + osmGeo.Tags.GetValue("addr:city")
+                    + ", " + osmGeo.Tags.GetValue("addr:postcode"));
 
-                Console.WriteLine(osmGeo.Tags.GetValue("postal_code"));
+                string postalCode = osmGeo.Tags.GetValue("addr:postcode");
+                int intPostalCode = 0;
+                if (int.TryParse(postalCode, out intPostalCode))
+                {
+                    // berlin postal codes are from 10115 to 14199
+                    if (intPostalCode >= 10115 && intPostalCode <= 14199)
+                        postalCodes.Add(postalCode);
+                }
             }
 
             postalCodes.Sort();
-
-            postalCodes.Distinct().ToList();
-
-            string combinedString = string.Join("\n", postalCodes);
-
+            List<string> distinctPostalCodes = postalCodes.Distinct().ToList();
+            string combinedString = string.Join("\n", distinctPostalCodes);
             File.WriteAllText("output.txt", combinedString);
+        }
+
+        /// <summary>
+        /// Downloads a file if it doesn't exist yet.
+        /// <summary>
+        public static async Task ToFile(string url, string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                var client = new HttpClient();
+                await using var stream = await client.GetStreamAsync(url);
+                await using var outputStream = File.OpenWrite(filename);
+                await stream.CopyToAsync(outputStream);
+            }
         }
     }
 }
