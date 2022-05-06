27c88d7988d613f373e9f1704a9671165dd6bddd
deema <damdom.94.8@gmail.com>
2022-05-06 16:08:52 +0200

    finishing

--- Program.cs
+++ Program.cs
@@ -27,42 +27,45 @@ namespace BerlinPostalCodes
             /// first tag "boundary=postal_code".
             /// based on taginfo's statistics this tag is useful only in Germany because
             /// the geographical distribution of this key here is very high.
-            /// "boundary=postal_code" will give us 192 postal codes in Berlin.
-            /// use linq to leave only objects with Type = Way and contains the tag "postal_code".
+            /// https://taginfo.openstreetmap.org/tags/boundary=postal_code?filter=all#map
+            /// "boundary=postal_code" will give us 192 postal codes in Berlin and 29 codes in Brandenburg.
+            /// use linq to leave only objects where "boundary"="postal_code".
             /// <summary>
             //var filtered = from osmGeo in source
-            //               where osmGeo.Tags.ContainsKey("postal_code") && osmGeo.Type == OsmSharp.OsmGeoType.Way
+            //               where osmGeo.Tags.Contains("boundary", "postal_code")
             //               select osmGeo;
 
             /// <summary>
-            /// second key "addr:postcode"
-            /// in Karlsruhe Schema they use this key
-            /// "addr:postcode=*" will give us 193 postal codes in Berlin
-            /// use linq to leave only objects with Type = Node and contains the key "addr:postcode"
+            /// second tag "addr:postcode".
+            /// in Karlsruhe Schema they use this tag.
+            /// https://taginfo.openstreetmap.org/keys/addr:postcode?filter=all#map
+            /// "addr:postcode=*" will give us 194 postal codes in Berlin and 3 codes in Brandenburg.
+            /// use linq to leave only objects with Type = Node and contains the tag "addr:postcode".
             /// <summary>
             var filtered = from osmGeo in source
                            where osmGeo.Tags.ContainsKey("addr:postcode") && osmGeo.Type == OsmSharp.OsmGeoType.Node
+                           && osmGeo.Tags.Contains("addr:city", "Berlin")
                            select osmGeo;
 
             /// <summary>
-            /// add postal codes to a list then sort it and finally remove duplicates
+            /// add postal codes to a list then sort it and finally remove duplicates.
             /// <summary>
             List<string> postalCodes = new List<string>();
             foreach (var osmGeo in filtered)
             {
-                // show details on console
+                // show details on console.
                 Console.WriteLine(osmGeo.Tags.GetValue("addr:street")
                     + ", " + osmGeo.Tags.GetValue("addr:city")
                     + ", " + osmGeo.Tags.GetValue("addr:postcode"));
 
                 string postalCode = osmGeo.Tags.GetValue("addr:postcode");
-                int intPostalCode = 0;
-                if (int.TryParse(postalCode, out intPostalCode))
-                {
-                    // berlin postal codes are from 10115 to 14199
-                    if (intPostalCode >= 10115 && intPostalCode <= 14199)
+                //int intPostalCode = 0;
+                //if (int.TryParse(postalCode, out intPostalCode))
+                //{
+                //    // berlin postal codes are 198 from "10115" to "14199".
+                //    if (intPostalCode >= 10115 && intPostalCode <= 14199)
                         postalCodes.Add(postalCode);
-                }
+                //}
             }
 
             postalCodes.Sort();
